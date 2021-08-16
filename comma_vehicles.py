cf8d9a0161c4ee470b34e0008f3b464a9ceb6ae7
Brandon Bennett <56660362+bbennett80@users.noreply.github.com>
2021-08-15 18:41:07 -0700

    Create comma_vehicles.py

---
+++ comma_vehicles.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+def comma_url():
+    return 'https://github.com/commaai/openpilot'
+
+def read_html(url):
+    return pd.read_html(url)
+
+def write_csv(*tables):
+    supported = pd.concat(tables, ignore_index=True)
+    supported.to_csv('comma_vehicles.csv', index=False)
+    return supported
+
+url = comma_url()
+read_page = read_html(url)
+csv = write_csv(read_page[1], read_page[2])
